52a02cd5615cbe347449cc289ff37a03c1101456
Alan <dasilva.alan@outlook.com>
2021-12-29 16:22:07 +0200

    added test

---
+++ todo-app/todo-frontend/src/Todos/Todo.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import Todo from './Todo';
+
+test('renders content', () => {
+  const todo = {
+    text: 'Component testing is done with react-testing-library',
+    done: false,
+  };
+
+  const component = render(<Todo todo={todo} />);
+
+  expect(component.container).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  );
+  expect(component.container).toHaveTextContent(
+    'Component testing is done as a jest'
+  );
+});
